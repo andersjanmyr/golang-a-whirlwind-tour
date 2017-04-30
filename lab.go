0f052ffd1f13f2f649b1bec10e6e56289ee86d5b
Anders Janmyr <anders@janmyr.com>
2017-04-29 21:18:50 -0700

    Added some more tests to implement

--- lab.go
+++ lab.go
@@ -19,3 +19,32 @@ func DoubleMultiplesOf(numbers []int, n int) []int {
 func Filter(strings []string, f func(string) bool) []string {
 	return []string{}
 }
+
+func Inc(counter *int) {
+}
+
+func Fibonacci() func() int {
+	return func() int {
+		return 0
+	}
+}
+
+type Sayer interface {
+	say() string
+}
+
+type Muter interface {
+	mute()
+}
+
+type Tapir struct {
+}
+
+func (tapir Tapir) say() string {
+	return ""
+}
+
+func (tapir Tapir) mute() {
+}
+
+// Implement Cow here
--- lab_test.go
+++ lab_test.go
@@ -17,9 +17,9 @@ func TestSwap(t *testing.T) {
 }
 
 func TestInitials(t *testing.T) {
-	var names = []string{"Kant", "Popper", "Aristotle", "Socrates", "Plato"}
-	var actual = Initials(names)
-	var expected = []string{"K", "P", "A", "S", "P"}
+	names := []string{"Kant", "Popper", "Aristotle", "Socrates", "Plato"}
+	actual := Initials(names)
+	expected := []string{"K", "P", "A", "S", "P"}
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: %v, actual: %v\n", expected, actual)
 	}
@@ -34,9 +34,9 @@ func TestSnakeCaseDasherized(t *testing.T) {
 }
 
 func TestDoubleMultiplesOf(t *testing.T) {
-	var numbers = []int{1, 3, 7, 9, 14, 22, 28}
-	var actual = DoubleMultiplesOf(numbers, 7)
-	var expected = []int{1, 3, 14, 9, 28, 22, 56}
+	numbers := []int{1, 3, 7, 9, 14, 22, 28}
+	actual := DoubleMultiplesOf(numbers, 7)
+	expected := []int{1, 3, 14, 9, 28, 22, 56}
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: %v, actual: %v\n", expected, actual)
 	}
@@ -53,3 +53,68 @@ func TestFilter(t *testing.T) {
 		t.Errorf("Expected: %v, actual: %v\n", expected, actual)
 	}
 }
+
+func TestIncrease(t *testing.T) {
+	i := 1
+	Inc(&i)
+	expected := 2
+	if i != expected {
+		t.Errorf("Expected %v, actual %v", expected, i)
+	}
+}
+
+func TestMap(t *testing.T) {
+	ascii := map[string]int{"A": 65, "B": 67}
+	// Write code here
+	if !reflect.DeepEqual(map[string]int{"A": 65, "B": 66}, ascii) {
+		t.Errorf("Expected %v, actual %v", map[string]int{"A": 65, "B": 66}, ascii)
+	}
+	// Write code here
+	if !reflect.DeepEqual(map[string]int{"A": 65}, ascii) {
+		t.Errorf("Expected %v, actual %v", map[string]int{"A": 65}, ascii)
+	}
+	// Write code here
+	if !reflect.DeepEqual(map[string]int{"A": 65, "C": 67}, ascii) {
+		t.Errorf("Expected %v, actual %v", map[string]int{"A": 65, "C": 67}, ascii)
+	}
+}
+
+func assert(expected, actual interface{}, t *testing.T) {
+	if actual != expected {
+		t.Errorf("Expected %v, actual %v", expected, actual)
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	f := Fibonacci()
+	assert(1, f(), t)
+	assert(1, f(), t)
+	assert(2, f(), t)
+	assert(3, f(), t)
+	assert(5, f(), t)
+	assert(8, f(), t)
+	assert(13, f(), t)
+	assert(21, f(), t)
+}
+
+func TestInterfaces(t *testing.T) {
+	tapir := Tapir{}
+	assert("oiiiiiiii", tapir.say(), t)
+	tapir.mute()
+	assert("nada", tapir.say(), t)
+}
+
+func speak(sayer Sayer) string {
+	return sayer.say()
+}
+
+func silence(muter Muter) {
+	muter.mute()
+}
+
+// func TestInterfaces2(t *testing.T) {
+// 	cow := Cow{}
+// 	assert("Mooo", cow.say(), t)
+// 	tapir.mute()
+// 	assert("nada", cow.say(), t)
+// }
